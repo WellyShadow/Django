6b9a187492bcde7281a2ba9dc89dd1b5a673a81a
Vladyslav Koshelenko <Vladyslav_Koshelenko@epam.com>
2023-02-01 12:31:31 +0200

    add layout

--- djangoproject/main/views.py
+++ djangoproject/main/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("<h4>Check working</h4>")
+    return render(request,'main/index.html')
 def about(request):
-    return HttpResponse("<h4>About</h4>")
+    return render(request,'main/about.html')
