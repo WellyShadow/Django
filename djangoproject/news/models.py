e8d5dec825f1f4031bed14145e40538931d2f54b
Vladyslav Koshelenko <Vladyslav_Koshelenko@epam.com>
2023-05-23 14:21:33 +0300

    make migrates with database

--- djangoproject/news/models.py
+++ djangoproject/news/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Artiles(models.Model):
+    title = models.CharField('Name', max_length=50)
+    announcement = models.CharField('Announcement', max_length=250)
+    full_text = models.TextField('Text')
+    date = models.DateTimeField('Date')
+    
+    def __str__(self):
+        return self.title
