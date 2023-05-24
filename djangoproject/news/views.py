e6a59d0190993135ff22b139faa2385d27c78fba
Vladyslav Koshelenko <Vladyslav_Koshelenko@epam.com>
2023-05-24 15:54:32 +0300

    sort info from db

--- djangoproject/news/views.py
+++ djangoproject/news/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from .models import Artiles
 # Create your views here.
 def news_home(request):
-    news = Artiles.objects.all()
+    news = Artiles.objects.order_by('-date')
     return render(request, 'news/news_home.html',{'news':news})
