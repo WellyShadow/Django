32e2b4deae421598b89a8bbe8ff03458af9f3ed1
Vladyslav Koshelenko <Vladyslav_Koshelenko@epam.com>
2023-05-25 13:10:33 +0300

    add create news page

--- djangoproject/news/views.py
+++ djangoproject/news/views.py
@@ -4,3 +4,5 @@ from .models import Artiles
 def news_home(request):
     news = Artiles.objects.order_by('-date')
     return render(request, 'news/news_home.html',{'news':news})
+def create(request):
+    return render(request,'news/create.html')
