3c0d590808e0f2338e9c534f6e11cf7663865732
Vladyslav Koshelenko <Vladyslav_Koshelenko@epam.com>
2023-05-25 14:44:24 +0300

    processing data from a form

--- djangoproject/news/views.py
+++ djangoproject/news/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Artiles
 from .forms import ArtilesForm
 # Create your views here.
@@ -6,8 +6,18 @@ def news_home(request):
     news = Artiles.objects.order_by('-date')
     return render(request, 'news/news_home.html',{'news':news})
 def create(request):
+    error=''
+    if request.method == 'POST':
+        form = ArtilesForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+        else:
+            error = 'Error Form'
+
     form = ArtilesForm()
     data ={
-        'form':form
+        'form':form,
+        'error':error
     }
     return render(request,'news/create.html',data)
