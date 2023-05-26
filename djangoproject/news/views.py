8b86ee9760444720b93484e20d362d0cc9ba73e8
Vladyslav Koshelenko <Vladyslav_Koshelenko@epam.com>
2023-05-26 12:39:31 +0300

    creat a class for processing templates

--- djangoproject/news/views.py
+++ djangoproject/news/views.py
@@ -1,10 +1,19 @@
 from django.shortcuts import render, redirect
 from .models import Artiles
 from .forms import ArtilesForm
+from django.views.generic import DetailView
 # Create your views here.
+
+
 def news_home(request):
     news = Artiles.objects.order_by('-date')
     return render(request, 'news/news_home.html',{'news':news})
+
+class NewsDetailView(DetailView):
+    model=Artiles
+    template_name='news/detail_view.html'
+    context_object_name = 'article'
+
 def create(request):
     error=''
     if request.method == 'POST':
